ed38b23b486f7ed67e19f5cad8448a3dc0a3f128
Ahmed <ahmedhania65@gmail.com>
2024-11-23 14:07:08 +0400

    Add files via upload

---
+++ exercise 03.py
@@ -0,0 +1,4 @@
+A = {"Name": "Ahmed", "Hometown": "Ras Al Khaimah", "Age": 18}
+
+for key, value in A.items():
+    print(f"{key}: {value}") 
---
+++ exercise 07.py
@@ -0,0 +1,22 @@
+print("Inc to 50")
+for x in range(51):
+        print(x)
+
+print("dec to 50")
+for x in range(51):
+        x = 50-x
+        print(x)
+
+print("thirty")
+for x in range(29,50):
+        x = x + 1
+        print(x)
+        
+print("dec to 50")
+for x in range(50,9,-2):
+        print(x)
+
+        
+print("hunnid")
+for x in range(100,205,5):
+        print(x)
